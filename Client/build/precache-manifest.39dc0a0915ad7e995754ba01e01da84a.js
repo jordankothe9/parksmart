9232a691689a971608b107549390796d767aadcd
jordankothe9 <jordankothe9@gmail.com>
2021-04-26 19:50:33 -0400

    new build

---
+++ Client/build/precache-manifest.39dc0a0915ad7e995754ba01e01da84a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f7d4361eb2e0ffa438ec039b64d813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "337a97ea4a608d32d0e6",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "dfba606adceff3153f7a",
+    "url": "/static/css/main.31186d7d.chunk.css"
+  },
+  {
+    "revision": "337a97ea4a608d32d0e6",
+    "url": "/static/js/2.e87971ab.chunk.js"
+  },
+  {
+    "revision": "609352ea6af9e0ac98be5b5fe36b1f6a",
+    "url": "/static/js/2.e87971ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfba606adceff3153f7a",
+    "url": "/static/js/main.6750e55a.chunk.js"
+  },
+  {
+    "revision": "f87f270af169b9b874c8",
+    "url": "/static/js/runtime-main.c4de9899.js"
+  },
+  {
+    "revision": "37b37828bf81af17e982b871c8fbeab6",
+    "url": "/static/media/ParkSmartBanner2.37b37828.png"
+  }
+]);
